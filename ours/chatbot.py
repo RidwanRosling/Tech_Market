38d6c55f843f1a15ab4fb9977469262c8eaef8a4
RidwanRosling <ridwansbdt@gmail.com>
2024-12-11 14:14:55 +0700

    delete def chat_bot cause it's not necessary

--- ours/chatbot.py
+++ ours/chatbot.py
@@ -36,27 +36,27 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
         if q["question"] == question:
             return q["answer"]
 
-def chat_bot():
-    knowledge_base: dict = load_knowledge_base('knowledge_base.json')
+# def chat_bot():
+#     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
-    while True:
-        user_input: str = input('You: ')
+#     while True:
+#         user_input: str = input('You: ')
 
-        if user_input.lower() == 'quit':
-            break
-        best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
+#         if user_input.lower() == 'quit':
+#             break
+#         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
 
-        if best_match:
-            answer: str = get_answer_for_question(best_match, knowledge_base)
-            print(f'Bot: {answer}')
-        else:
-            print('Bot: I don\'t know the answer. Can you teach me?')
-            new_answer: str = input('Type the answer or "skip" to skip: ')
+#         if best_match:
+#             answer: str = get_answer_for_question(best_match, knowledge_base)
+#             print(f'Bot: {answer}')
+#         else:
+#             print('Bot: I don\'t know the answer. Can you teach me?')
+#             new_answer: str = input('Type the answer or "skip" to skip: ')
 
-            if new_answer.lower() != 'skip':
-                knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
-                save_knowledge_base('knowledge_base.json', knowledge_base)
-                print('Bot: Thank you! I learned a new response!')
+#             if new_answer.lower() != 'skip':
+#                 knowledge_base["questions"].append({"question": user_input, "answer": new_answer})
+#                 save_knowledge_base('knowledge_base.json', knowledge_base)
+#                 print('Bot: Thank you! I learned a new response!')
 
 def load_pc_recommendations(file_path: str) -> dict:
     # Pastikan path file yang dimasukkan sesuai dengan lokasi
