60226ccadac4f0439840270c24a4ee4df201bba3
User-CruZer <ahmadzakialbadri@gmail.com>
2024-12-19 22:08:06 +0700

    added pic on about us

--- ours/tentang_toko.py
+++ ours/tentang_toko.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from data_deskrip import *
 
+left, mid, right = st.columns(3)
+
+with mid:
+    st.image("ours/kumpulan gambar/Store.png", use_container_width=True)
+
 st.title("About Us!")
 
 st.write("Salah satu alasan utama mengapa merakit PC adalah pilihan terbaik untuk jangka\
